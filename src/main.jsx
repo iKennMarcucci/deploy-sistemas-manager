2048cad89a58febfbac89312e39bc4355a8b98eb
Kenn Marcucci <marcucci5783@gmail.com>
2025-06-03 16:41:53 -0500

    fix: enable React.StrictMode for improved development experience

--- src/main.jsx
+++ src/main.jsx
@@ -8,7 +8,7 @@ import { GoogleOAuthProvider } from '@react-oauth/google'
 const clientId = import.meta.env.VITE_CLIENT_ID_GOOGLE
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  // <React.StrictMode>
+  <React.StrictMode>
     <GoogleOAuthProvider clientId={clientId}>
       <BrowserRouter>
         <HeroUIProvider>
@@ -16,5 +16,5 @@ ReactDOM.createRoot(document.getElementById('root')).render(
         </HeroUIProvider>
       </BrowserRouter>
     </GoogleOAuthProvider>
-  // </React.StrictMode>
+  </React.StrictMode>
 )
